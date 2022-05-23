a8c49d64db4eb010e70e9a7978eb247d9f601fa3
Hunter-Bennett <52138430+Hunter-Bennett@users.noreply.github.com>
2022-05-23 18:13:35 -0400

    Add files via upload

---
+++ web/main/views.py
@@ -0,0 +1,20 @@
+from re import template
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+    
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'accounts/profile.html'
